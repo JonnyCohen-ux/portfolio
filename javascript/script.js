0098d0306880a7623595c34afc75ea92ac992c07
JonnyCohen-ux <you@example.com>
2020-12-12 20:51:13 +0200

    new portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
+const navbar = document.querySelector("nav");
 window.addEventListener("scroll", function () {
-  const navbar = document.querySelector("nav");
   navbar.classList.toggle("sticky", window.scrollY > 25);
 });
 
@@ -9,3 +9,23 @@ colorSwitcherButton.addEventListener("click", () => {
   const colorSwitcher = document.querySelector(".colorSwitcher");
   colorSwitcher.classList.toggle("openColorSwitcher");
 });
+
+const burgerButton = document.querySelector(".burgermenuButton");
+burgerButton.addEventListener("click", () => {
+  const repsonsiveNavbar = document.querySelector(".responsiveNavLinks");
+  repsonsiveNavbar.classList.toggle("openResposiveNav");
+});
+
+// Set Active Link Class
+const navLinks = document.querySelector(".navLinks"),
+  navLinksLi = document.querySelectorAll("li");
+for (let i = 0; i < navLinksLi.length; i++) {
+  const aLinks = navLinksLi[i].querySelector("a");
+  aLinks.addEventListener("click", () => {
+    for (let j = 0; j < navLinksLi.length; j++) {
+      let link = navLinksLi[j].querySelector("a");
+      link.classList.remove("active");
+    }
+    aLinks.classList.add("active");
+  });
+}
