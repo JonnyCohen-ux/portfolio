6da808bf00eb7c7b0ceb41639c44384a9e81c61a
JonnyCohen-ux <you@example.com>
2020-12-12 21:21:03 +0200

    loader

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden";
+});
+
 const navbar = document.querySelector("nav");
 window.addEventListener("scroll", function () {
   navbar.classList.toggle("sticky", window.scrollY > 25);
